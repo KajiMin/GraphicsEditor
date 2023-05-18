5c75f5429002aaef1c0c1dc2bd8ab2e09ec3312d
Сергей Казьмин <kazmin-serega2002.kazmin@yandex.ru>
2023-05-18 22:09:28 +0700

    Добавьте файлы проекта.

---
+++ GraphicsEditor/Views/Shape2_UserControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GraphicsEditor.Views
+{
+    public partial class Shape2_UserControl : UserControl
+    {
+        public Shape2_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
